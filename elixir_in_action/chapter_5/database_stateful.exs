887c0d143cefa2ad382bdeff75aeb43f3b167458
Luis Geniole <luis.geniole@gmail.com>
2021-04-10 21:10:45 -0300

    Add stateful database.

--- elixir_in_action/chapter_5/database_stateful.exs
+++ elixir_in_action/chapter_5/database_stateful.exs
@@ -2,7 +2,10 @@ defmodule DatabaseServer do
   @query_time 2000
 
   def start do
-    spawn(&loop/0)
+    spawn(fn ->
+      connection = :rand.uniform(1000)
+      loop(connection)
+    end)
   end
 
   def run_async(pid, query_def), do: send(pid, {:run_query, self(), query_def})
@@ -15,32 +18,27 @@ defmodule DatabaseServer do
     end
   end
 
-  defp loop do
+  defp loop(connection) do
     receive do
-      {:run_query, caller, query_def} -> send(caller, {:query_result, run_query(query_def)})
+      {:run_query, caller, query_def} ->
+        query_result = run_query(connection, query_def)
+        send(caller, {:query_result, query_result})
     end
 
-    loop()
+    loop(connection)
   end
 
-  defp run_query(query_def) do
+  defp run_query(connection, query_def) do
     Process.sleep(@query_time)
-    "#{query_def} result"
+    "Connection #{connection}: #{query_def} result"
   end
 end
 
 pid = DatabaseServer.start()
-send(pid, {:run_query, self(), "query 1"})
-
-receive do
-  {:query_result, result} -> IO.puts(result)
-end
-
+DatabaseServer.run_async(pid, "query 1")
+DatabaseServer.get_result()
 DatabaseServer.run_async(pid, "query 2")
 DatabaseServer.get_result()
-
-Enum.map(1..5, &DatabaseServer.run_async(pid, "query #{&1}"))
-Enum.map(1..5, fn _ -> DatabaseServer.get_result() end)
 
 pool = Enum.map(1..100, fn _ -> DatabaseServer.start() end)
 
